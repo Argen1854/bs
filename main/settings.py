9aa1885613c87d1a630a44a411c8f91d85e55fba
Argen <argen1854@gmail.com>
2022-03-20 15:13:10 +0300

    2

--- main/settings.py
+++ main/settings.py
@@ -128,9 +128,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = 'media/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
